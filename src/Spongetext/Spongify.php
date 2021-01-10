499bac9a5af397167aced0cb162e6ae04a401b30
Sam Huynh <samhwang2112.dev@gmail.com>
2021-01-11 09:29:59 +1100

    Reformat with phpcbf

--- src/Spongetext/Spongify.php
+++ src/Spongetext/Spongify.php
@@ -4,26 +4,26 @@ namespace Spongetext;
 
 class Spongify
 {
-  public function spongify(string $sentence): string
-  {
-    $sentence = trim($sentence);
-    $sentence = strtolower($sentence);
-    $characters = str_split($sentence, 1);
-    $output = array_reduce($characters, "self::spongifyCallback", '');
+    public function spongify(string $sentence): string
+    {
+        $sentence = trim($sentence);
+        $sentence = strtolower($sentence);
+        $characters = str_split($sentence, 1);
+        $output = array_reduce($characters, "self::spongifyCallback", '');
 
-    return $output;
-  }
+        return $output;
+    }
 
-  protected function spongifyCallback(string $outputText, string $character): string
-  {
-    $randomBoolean = $this->getRandomBoolean();
-    $spongeCharacter = $randomBoolean ? strtoupper($character) : $character;
+    protected function spongifyCallback(string $outputText, string $character): string
+    {
+        $randomBoolean = $this->getRandomBoolean();
+        $spongeCharacter = $randomBoolean ? strtoupper($character) : $character;
 
-    return $outputText . $spongeCharacter;
-  }
+        return $outputText . $spongeCharacter;
+    }
 
-  protected function getRandomBoolean(): bool
-  {
-    return (bool)random_int(0, 1);
-  }
+    protected function getRandomBoolean(): bool
+    {
+        return (bool)random_int(0, 1);
+    }
 }
--- tests/Spongetext/SpongifyTest.php
+++ tests/Spongetext/SpongifyTest.php
@@ -7,32 +7,32 @@ use Faker\Factory as Faker;
 
 class SpongifyTest extends TestCase
 {
-  protected function getInputAndOutput(): array
-  {
-    $faker = Faker::create('en_AU');
-    $sponge = new Spongify();
+    protected function getInputAndOutput(): array
+    {
+        $faker = Faker::create('en_AU');
+        $sponge = new Spongify();
     
-    $input = $faker->sentence(10);
-    $output = $sponge->spongify($input);
+        $input = $faker->sentence(10);
+        $output = $sponge->spongify($input);
 
-    return [$input, $output];
-  }
+        return [$input, $output];
+    }
 
-  public function testSpongeNotNull(): void
-  {
-    [$_input, $output] = $this->getInputAndOutput();
-    $this->assertNotNull($output);
-  }
+    public function testSpongeNotNull(): void
+    {
+        [$_input, $output] = $this->getInputAndOutput();
+        $this->assertNotNull($output);
+    }
 
-  public function testSpongeReturnsString(): void
-  {
-    [$_input, $output] = $this->getInputAndOutput();
-    $this->assertIsString($output);
-  }
+    public function testSpongeReturnsString(): void
+    {
+        [$_input, $output] = $this->getInputAndOutput();
+        $this->assertIsString($output);
+    }
 
-  public function testSpongeSameLengthAsinput(): void
-  {
-    [$input, $output] = $this->getInputAndOutput();
-    $this->assertEquals(strlen($input), strlen($output));
-  }
+    public function testSpongeSameLengthAsinput(): void
+    {
+        [$input, $output] = $this->getInputAndOutput();
+        $this->assertEquals(strlen($input), strlen($output));
+    }
 }
